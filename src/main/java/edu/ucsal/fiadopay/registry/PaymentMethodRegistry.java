dd83db8fb551188c4e46de44c937ac5f89151898
Pedro Silva <pedro.o.silva975@gmail.com>
2025-11-20 18:11:26 -0300

    feat: aplicando o padrão Registry para manter os PaymentProviders disponíveis para a PaymentService

---
+++ src/main/java/edu/ucsal/fiadopay/registry/PaymentMethodRegistry.java
@@ -0,0 +1,39 @@
+package edu.ucsal.fiadopay.registry;
+
+import edu.ucsal.fiadopay.annotation.PaymentMethod;
+import edu.ucsal.fiadopay.provider.payment.PaymentProvider;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class PaymentMethodRegistry {
+
+    private final Map<String, PaymentProvider> paymentProviders = new HashMap<>();
+    private final ApplicationContext applicationContext;
+
+    public PaymentMethodRegistry(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @PostConstruct
+    public void setProcessors() {
+
+        Map<String, Object> beans = applicationContext.getBeansWithAnnotation(PaymentMethod.class);
+
+        for(Object bean : beans.values()) {
+            PaymentMethod paymentMethod = bean.getClass().getAnnotation(PaymentMethod.class);
+            if (paymentMethod != null && bean instanceof PaymentProvider) {
+                paymentProviders.put(paymentMethod.paymentType(), (PaymentProvider) bean);
+            }
+        }
+    }
+
+    public PaymentProvider getProvider(String method) {
+        return paymentProviders.getOrDefault(method.toUpperCase(), paymentProviders.get("DEFAULT"));
+    }
+
+}
